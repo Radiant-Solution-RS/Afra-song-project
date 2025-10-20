daca6b5219a8d301e4d21e710cf804ca584de4d5
Radiant-Solution-RS <aawais.github@gmail.com>
2025-10-20 18:35:05 +0500

    update all changes

--- Afra Tabs final/tabs/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TabsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'  # type: ignore
-    name = 'tabs'
-
-    def ready(self):
-        import tabs.signals
